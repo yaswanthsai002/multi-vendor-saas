6aba208058856e3497789eef61ffe9956d40eee3
Yaswanth Sai Kotagiri <yaswanthsaikotagiri1@gmail.com>
2026-09-08 20:12:34 +0530

    feat: enhance header components with delivery address, user menu, and search functionality

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
