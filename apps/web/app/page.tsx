941b4468a315d36678193ac15d7e2ba89facb820
Yaswanth Sai Kotagiri <yaswanthsaikotagiri1@gmail.com>
2026-09-17 17:29:28 +0530

    refactor: remove TopPicks component and clean up imports in Home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,6 @@ import FeaturedBrands from '@/features/home/components/FeaturedBrands';
 import Hero from '@/features/home/components/Hero';
 import NewArrivals from '@/features/home/components/NewArrivals';
 import ShopbyCategory from '@/features/home/components/ShopbyCategory';
-// import TopPicks from '@/features/home/components/TopPicks';
 import WhyPerigee from '@/features/home/components/WhyPerigee';
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       <ShopbyCategory />
       <NewArrivals />
       <FeaturedBrands />
-      {/* <TopPicks /> */}
       <Discovery />
       <WhyPerigee />
     </>
